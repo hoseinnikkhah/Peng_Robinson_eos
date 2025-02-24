7825f233d30df1c2fbd3d86922f1725015dfa2cf
Hosein Nikkhah <hosein_nikkhah@chemeng.iust.ac.ir>
2025-02-24 17:49:13 +0000

    Feb 24, 2025, 9:19 PM

--- 328Kelvin.m
+++ 328Kelvin.m
@@ -1,12 +1,4 @@
-P = 50;
-T = 253;
-V_m = 300;
-R = 8.314;
+T_c = 253;
 
-Z_emprical = (P*V_m)/(R*T);
 
-Mw_drug = 598.54;
-Mw_CO2 = 44;
-V_c = 0.5;       $ [L] converted from 500 micro liter
-V_s = 1;         $ There is no data about this however we can assume it to be somewhere around 1 mL to 10 mL
-
+P = [120 150 180 210 240 270]
