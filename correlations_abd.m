2ca8d7ab26806a06bba6e00d76ecf5f5a7b98da3
Hosein Nikkhah <hosein_nikkhah@chemeng.iust.ac.ir>
2025-02-24 07:30:05 +0000

    Feb 24, 2025, 11:00 AM

--- correlations_abd.m
+++ correlations_abd.m
@@ -1,4 +1,6 @@
 function [a, b, c ] = correlations_abd(T_c, P_c, T, m)
     R = 8.314;      [J/mol·K]
 
-    a = 0.457235
+    a = 0.457235 * (((R^2)*(T_c^2))/P_c) * (1 + m*(1 - sqrt(T/T_c)))^2 * (1 + m)^2 
+    
+    
