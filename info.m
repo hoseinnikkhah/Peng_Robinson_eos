f23d6474a69a3e323533fb39120333b34dc14eec
Hosein Nikkhah <hosein_nikkhah@chemeng.iust.ac.ir>
2025-02-24 19:42:04 +0000

    Feb 24, 2025, 11:12 PM

--- info.m
+++ info.m
@@ -17,7 +17,7 @@ y_1 =   [2.03 2.32 2.48 3.33 3.80 5.32;
 y_b =   [2.03 2.32 2.48 3.33 3.80 5.32;
         1.63 2.11 3.12 3.61 4.30 5.96;
         1.29 1.96 3.76 4.08 5.36 7.22;
-        0.90 1.52 4.39 4.94 6.10 8.01]*10^6;        % mole fraction
+        0.90 1.52 4.39 4.94 6.10 8.01];        % mole fraction
 
 S = zeros(4,6);
 for j=1:4
@@ -55,4 +55,3 @@ title('The solubility data of Ceftriaxone sodium drug in SC-CO2');
 
 legend('T = 308 K', 'T = 318 K', 'T = 328 K', 'T = 338 K');
 xlim([115 280]);
-ytickformat('%.2f');
