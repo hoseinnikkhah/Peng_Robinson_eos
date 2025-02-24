04ed9b8b58c7ee20a3f27f71a95576fb86fe4d14
Hosein Nikkhah <hosein_nikkhah@chemeng.iust.ac.ir>
2025-02-24 07:26:41 +0000

    Feb 24, 2025, 10:56 AM

--- correlations_abd.m
+++ correlations_abd.m
@@ -1,6 +1,4 @@
+function [a, b, c ] = correlations_abd(T_c, P_c, T, m)
+    R = 8.314;      [J/mol·K]
 
-R = 8.314;          $ Gas constant [J/mol·K]
-T_c = 256;          $ Cold Temp [K]
-P_c = 50;           $ Pressure at Cold temp [bar]
-
-a = 0.457235
+    a = 0.457235
