dfb68af5025ed49e8d6095292e748b549bd56cda
Hosein Nikkhah <hosein_nikkhah@chemeng.iust.ac.ir>
2025-02-24 17:37:30 +0000

    Feb 24, 2025, 9:07 PM

--- main.m
+++ main.m
@@ -7,4 +7,6 @@ Z_emprical = (P*V_m)/(R*T);
 
 Mw_drug = 598.54;
 Mw_CO2 = 44;
-V_c = 0.0005;       $ [L] converted from 500 micro liter
+V_c = 0.5;       $ [L] converted from 500 micro liter
+V_s = 1;         $ There is no data about this however we can assume it to be somewhere around 1 mL to 10 mL
+
