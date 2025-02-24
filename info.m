e1f45ac7afebc21c37380106b11b7e10e377cd07
Hosein Nikkhah <hosein_nikkhah@chemeng.iust.ac.ir>
2025-02-24 19:40:51 +0000

    Feb 24, 2025, 11:10 PM

--- info.m
+++ info.m
@@ -12,7 +12,12 @@ rho_CO2 =   [769 817 849 875 896 914;
 y_1 =   [2.03 2.32 2.48 3.33 3.80 5.32;
         1.63 2.11 3.12 3.61 4.30 5.96;
         1.29 1.96 3.76 4.08 5.36 7.22;
-        0.90 1.52 4.39 4.94 6.10 8.01]*10^6;       % mole fraction
+        0.90 1.52 4.39 4.94 6.10 8.01]/10^6;        % mole fraction
+
+y_b =   [2.03 2.32 2.48 3.33 3.80 5.32;
+        1.63 2.11 3.12 3.61 4.30 5.96;
+        1.29 1.96 3.76 4.08 5.36 7.22;
+        0.90 1.52 4.39 4.94 6.10 8.01]*10^6;        % mole fraction
 
 S = zeros(4,6);
 for j=1:4
@@ -20,16 +25,29 @@ for j=1:4
         S(j,i) = (rho_CO2(j,i) * y_1(j,i) * Mw_drug)/(Mw_CO2 * (1 - y_1(j,i)));
     end
 end
-
+S = S*100
 % C_drug = [0.021 0.026 0.029 0.039 0.046 0.066];     % solubility or concentration [g/L]
 
-figure;
+figure(1);
 hold on;
-
 scatter(P, S(1,:), 'r', 'o', 'MarkerFaceColor', 'r');  
 scatter(P, S(2,:), 'g', 's', 'MarkerFaceColor', 'g');  
 scatter(P, S(3,:), 'b', 'd', 'MarkerFaceColor', 'b');  
 scatter(P, S(4,:), 'm', '^', 'MarkerFaceColor', 'm');  
+
+xlabel('Pressure [bar]');
+ylabel('Drug solubility');
+title('The solubility data of Ceftriaxone sodium drug in SC-CO2');
+
+legend('T = 308 K', 'T = 318 K', 'T = 328 K', 'T = 338 K');
+xlim([115 280]);
+
+figure(2);
+hold on;
+scatter(P, y_b(1,:), 'r', 'o', 'MarkerFaceColor', 'r');  
+scatter(P, y_b(2,:), 'g', 's', 'MarkerFaceColor', 'g');  
+scatter(P, y_b(3,:), 'b', 'd', 'MarkerFaceColor', 'b');  
+scatter(P, y_b(4,:), 'm', '^', 'MarkerFaceColor', 'm');  
 
 xlabel('Pressure [bar]');
 ylabel('Drug solubility');
