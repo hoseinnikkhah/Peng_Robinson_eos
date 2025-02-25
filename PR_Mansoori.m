fe835d644e8f37781b5eab88ff28d103ae1ff0fb
Hosein Nikkhah <hosein_nikkhah@chemeng.iust.ac.ir>
2025-02-25 12:47:13 +0000

    Feb 25, 2025, 4:17 PM

--- PR_Mansoori.m
+++ PR_Mansoori.m
@@ -3,6 +3,7 @@ Mw_drug = 598.5;
 Mw_CO2 = 44.01;
 Mw_methanol = 32.04;
 T = 308;
+P = 120;
 
 % The thermodynamic properties of Ceftriaxone sodium
 T_c_drug = 304.18;      % Temp [K]
@@ -14,4 +15,4 @@ T_c_CO2 = 1149.5;       % Temp [K]
 P_c_CO2 = 10.57;        % Pressure [bar]
 omega_CO2 = 2.0964;
 
-correlations(T_c_CO2, P_c_CO2, T, omega_CO2);
+correlations(T_c_CO2, P_c_CO2, T, P, omega_CO2);
