9869db098cd5afd08e397102d2eb3686f07b317f
Hosein Nikkhah <hosein_nikkhah@chemeng.iust.ac.ir>
2025-02-26 13:50:12 +0000

    Feb 26, 2025, 5:20 PM

--- PR_Mansoori.m
+++ PR_Mansoori.m
@@ -29,10 +29,13 @@ for i=1:4
 end
 
 % coefficents
-c3 = 1;
+Z = zeros(1,24);
 for i=1:24
+    c3 = 1;
     c2 = data_drug(2,i) - 1;
     c1 = data_drug(3,i) - 3*data_drug(2,i) + data_drug(1,i) - 2*sqrt(data_drug(1,i)*data_drug(3,i));
     c0 = data_drug(2,i)^3 + data_drug(2,i)^2 - data_drug(1,i)*data_drug(2,i) - data_drug(2,i)*data_drug(3,i) + 2*data_drug(2,i)*sqrt(data_drug(1,i)*data_drug(3,i));
-
+    coeff = [c3, c2, c1, c0];
+    Z(1,i) = roots(coeff);
+end
 
