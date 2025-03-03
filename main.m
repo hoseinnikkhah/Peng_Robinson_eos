88940a25c1f48bd9dd70c51ded477d43f38407ed
Hosein Nikkhah <hosein_nikkhah@chemeng.iust.ac.ir>
2025-03-03 15:19:24 +0000

    Mar 3, 2025, 6:49 PM

--- main.m
+++ main.m
@@ -3,5 +3,5 @@ T = 308;
 for i=1:300
     PR_method_function(T,P(i));
     system('filter_mole_fractions.py');
-    fprintf('Python process at P = %d Bar and T = %d Kelvin finished.\n', i, T);
+    fprintf('<a href="matlab:" style="color:blue">Python process</a> at <a href="matlab:" style="color:red">P = %d Bar</a> and <a href="matlab:" style="color:darkgreen">T = %d Kelvin</a> finished.\n', i, T);
 end
