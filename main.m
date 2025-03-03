e12a5775429947e456be55ca83e897334cfc60b5
Hosein Nikkhah <hosein_nikkhah@chemeng.iust.ac.ir>
2025-03-03 14:57:49 +0000

    Mar 3, 2025, 6:27 PM

--- main.m
+++ main.m
@@ -3,5 +3,5 @@ T = 308;
 for i=1:300
     PR_method_function(T,P(i));
     system('filter_mole_fractions.py');
+    fprintf('\033[34mPython process\033[0m at \033[31mP = %d Bar\033[0m and \033[32mT = %d Kelvin\033[0m finished.\n', i, T);
 end
-
