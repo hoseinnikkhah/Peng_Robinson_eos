beb212d0d30b030fa6e2466f52bfa8092fc16a4c
Hosein Nikkhah <hosein_nikkhah@chemeng.iust.ac.ir>
2025-03-03 16:32:10 +0000

    Mar 3, 2025, 8:02 PM

---
+++ PR_KM.m
@@ -0,0 +1,10 @@
+% SC CO2 Thermo info
+T_c_CO2 = 304.18;       % Critical Temp [Kelvin]
+P_c_CO2 = 73.8;         % Critical Pressure [bar]
+omega_CO2 = 0.225;      % Omega [-]
+
+% Drug Thermo info
+T_c_drug = 1149.5;      % Critical Temp [Kelvin]
+P_c_drug = 10.57;       % Critical Pressure [bar]
+omega_drug = 2.0964;    % Omega [-]
+
