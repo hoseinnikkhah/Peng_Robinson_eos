28b0fe4617a5c4d8ca1c70c43d1e98b0b9073be9
Hosein Nikkhah <hosein_nikkhah@chemeng.iust.ac.ir>
2025-03-03 18:09:33 +0000

    Mar 3, 2025, 9:39 PM

--- mixing_rules.m
+++ mixing_rules.m
@@ -1,22 +1,23 @@
 function [a_m, b_m, d_m] = mixing_rules(y_1, y_2, a_ij, b_ij, d_ij, n)
-
+    % a_m calculations
     a_term_1 = (y_1^2)*(a_ij(1,1,n)^(2/3))*(b_ij(1,1,n)^(1/3));
     a_term_2 = 2*y_1*y_2*(a_ij(1,2,n)^(2/3))*(b_ij(1,2,n)^(1/3));
     a_term_3 = (y_2^2)*(a_ij(2,2,n)^(2/3))*(b_ij(2,2,n)^(1/3));
     a_term_4 = sqrt(((y_1^2)*b_ij(1,1,n)) + (2*y_1*y_2*b_ij(1,2,n)) + ((y_2^2)*b_ij(2,2,n)));
-
+    % ---
     a_m = ((a_term_1 + a_term_2 + a_term_3)^(1.5))/a_term_4;
 
+    % b_m calculations
     b_term_1 = (y_1^2)*b_ij(1,1,n);
     b_term_2 = 2*y_1*y_2*b_ij(1,2,n);
     b_term_3 = (y_2^2)*b_ij(2,2,n);
-
+    % ---
     b_m = b_term_1 + b_term_2 + b_term_3;
-
+    % d_m calculations
     d_term_1 = (y_1^2)*d_ij(1,1,n);
     d_term_2 = 2*y_1*y_2*d_ij(1,2,n);
     d_term_3 = (y_2^2)*d_ij(2,2,n);
-
+    % ---
     d_m = d_term_1 + d_term_2 + d_term_3;
 
 end
