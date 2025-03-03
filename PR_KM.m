750d80bf671d7172de50534eeef14e97a21247cb
Hosein Nikkhah <hosein_nikkhah@chemeng.iust.ac.ir>
2025-03-03 16:33:06 +0000

    Mar 3, 2025, 8:03 PM

--- PR_KM.m
+++ PR_KM.m
@@ -8,3 +8,6 @@ T_c_drug = 1149.5;      % Critical Temp [Kelvin]
 P_c_drug = 10.57;       % Critical Pressure [bar]
 omega_drug = 2.0964;    % Omega [-]
 
+% Genral info
+k_ij = [0.103 0.117 0.126 0.134];   % Binary interaction parameter for PR/KM EoS
+R = 8.314;                          % Gas Constant []
