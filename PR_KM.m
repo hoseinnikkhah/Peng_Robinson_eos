9f02296bdeaa19a322f96b51ff8d5ee7c48ff4ce
Hosein Nikkhah <hosein_nikkhah@chemeng.iust.ac.ir>
2025-03-03 17:07:08 +0000

    Mar 3, 2025, 8:37 PM

--- PR_KM.m
+++ PR_KM.m
@@ -18,3 +18,8 @@ Mw_drug = 598.5;
 Mw_CO2 = 44.01;
 Mw_methanol = 32.04;
 
+correlations_CO2 = zeros(4,3);
+correlations_drug = zeros(4,3);
+
+for i=1:4
+    [a,b,d] = correlations[T_c_CO2, P_c_CO2, T(i), omega_CO2]
