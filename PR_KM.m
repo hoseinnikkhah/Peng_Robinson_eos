ff6b27c3c0fdedb254029ffc4fd4359bbd07ebde
Hosein Nikkhah <hosein_nikkhah@chemeng.iust.ac.ir>
2025-03-03 16:34:49 +0000

    Mar 3, 2025, 8:04 PM

--- PR_KM.m
+++ PR_KM.m
@@ -9,5 +9,11 @@ P_c_drug = 10.57;       % Critical Pressure [bar]
 omega_drug = 2.0964;    % Omega [-]
 
 % Genral info
+T = [308 318 328 338];              % Temp range
+P = 1:1:300;                        % Pressure range from 1 to 300 bar with step of 1
 k_ij = [0.103 0.117 0.126 0.134];   % Binary interaction parameter for PR/KM EoS
-R = 8.314;                          % Gas Constant []
+R = 8.314;                          % Gas Constant [J/mol·K]
+
+Mw_drug = 598.5;
+Mw_CO2 = 44.01;
+Mw_methanol = 32.04;
