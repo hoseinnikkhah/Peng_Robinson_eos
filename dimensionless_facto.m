3c9db6f5c43c241178c8b9d49146fc30920eda59
Hosein Nikkhah <hosein_nikkhah@chemeng.iust.ac.ir>
2025-03-03 20:29:08 +0000

    Mar 3, 2025, 11:59 PM

--- dimensionless_facto.m
+++ dimensionless_facto.m
@@ -1,8 +1,8 @@
-function [A, B, D] = dimensionless_factor(T, P, a, b, d)
+function [A, B, D] = dimensionless_factor(mixing_correlations, P, T, n)
     R = 8.314;      % [J/mol·K]
 
-    A = (a*P)/((R*T)^2);
-    B = (b*P)/(R*T);
-    D = (d*P)/(R*T);
+    A = (mixing_correlations(1,j,n)*P)/((R*T)^2);
+    B = (mixing_correlations(2,j,n)*P)/(R*T);
+    D = (mixing_correlations(3,j,n)*P)/(R*T);
 end
 
