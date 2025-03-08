a4fd248a473be8c5ea2e4698d74b135e949261c3
Hosein Nikkhah <hosein_nikkhah@chemeng.iust.ac.ir>
2025-03-08 18:56:02 +0000

    Mar 8, 2025, 10:26 PM

--- phi_calculation.m
+++ phi_calculation.m
@@ -1,1 +1,11 @@
-function 
+function [phi] = phi_calculation(y_1, y_2, b_m, T, Z, B)
+    R = 8.314;
+
+    term1 = ((2*((y_1*b_11) + (y_2*b_12)))/b_m) - 1;
+    term2 = tau/(sqrt(2)*R*T*b_m);
+    term3 = Z + (1 + sqrt(2))*B;
+    term4 = Z + (1 - sqrt(2))*B;
+
+    ln_phi = (Z-1)*term1 - log(Z - B) - term2*log(term3/term4);
+    phi = exp(ln_phi);
+end
