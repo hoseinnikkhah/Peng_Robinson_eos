885caf55dd4870e14ee4e3b6c1ff111b417e7f8b
Hosein Nikkhah <hosein_nikkhah@chemeng.iust.ac.ir>
2025-03-09 17:43:45 +0000

    Mar 9, 2025, 9:13 PM

--- PR_KM.m
+++ PR_KM.m
@@ -193,12 +193,11 @@ vSolid_1 = Mw_drug/rho_drug;
 vSolid_1 = vSolid_1 * 10^6; % [m^3/mol]
 
 new_y_1 = zeros(300,1000,4);
-for temp 1:4
+for temp=1:4
     for p=1:300
         for i=1:mole_length
-
             EXP = (vSolid_1*(p-PSub_1(temp)))/(R*T(temp));
-            new_y_1 = ((phi_sat*PSub_1(temp))*exp(EXP))/(p*phi_sat);
+            new_y_1(p,i,temp) = ((phi_sat*PSub_1(temp))*exp(EXP))/(p*phi_sat);
         end
     end
 end
