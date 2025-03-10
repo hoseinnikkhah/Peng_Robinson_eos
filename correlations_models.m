ff33ad9d6bda6dcecb62eeb62053843067dca38c
Hosein Nikkhah <hosein_nikkhah@chemeng.iust.ac.ir>
2025-03-10 06:58:07 +0000

    Mar 10, 2025, 10:28 AM

---
+++ correlations_models.m
@@ -0,0 +1,29 @@
+% SC CO2 Thermo info
+T_c_CO2 = 304.18;       % Critical Temp [Kelvin]
+P_c_CO2 = 73.8;         % Critical Pressure [bar]
+omega_CO2 = 0.225;      % Omega [-]
+
+% Drug Thermo info
+T_c_drug = 1149.5;      % Critical Temp [Kelvin]
+P_c_drug = 10.57;       % Critical Pressure [bar]
+omega_drug = 2.0964;    % Omega [-]
+
+% Genral info
+T = [308 318 328 338];              % Temp range
+P = 1:1:300;                        % Pressure range from 1 to 300 bar with step of 1
+k_ij = [0.103 0.117 0.126 0.134];   % Binary interaction parameter for PR/KM EoS
+R = 8.314;                          % Gas Constant [J/mol·K]
+
+Mw_drug = 598.5;
+Mw_CO2 = 44.01;
+Mw_methanol = 32.04;
+
+% KJ Model info
+a = -2.4761;
+b = 5.2409*10^3;
+c = -4550.5306;
+
+% GM Model info
+a = -2.4761;
+b = 5.2409*10^3;
+c = -4550.5306;
